06f5f8a5e36c02ee46073e04ed12e12b50da6603
lianaluci <llgrobstein@gmail.com>
2015-11-21 22:00:06 -0500

    complete part C. exercises, concatenate

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -29,11 +29,25 @@
     NSString *inString = @"in";
     NSString *yer = @"yer";
     NSString *sails = @"sails";
-    
     NSLog(@"%@ %@ %@ %@!", wind, inString, yer, sails);
     
     NSString *windInYerSails = [NSString stringWithFormat:@"%@ %@ %@ %@!", wind, inString, yer, sails];
     NSLog(@"%@", windInYerSails);
+    
+    NSLog(@"%@", wind);
+    windInYerSails = [ NSString stringWithString:wind];
+    NSLog(@"%@", windInYerSails);
+    
+    windInYerSails = [windInYerSails stringByAppendingString:@" "];
+    windInYerSails = [windInYerSails stringByAppendingString:inString];
+    NSLog(@"%@", windInYerSails);
+    windInYerSails  = [windInYerSails stringByAppendingFormat:@" %@", yer];
+    NSLog(@"%@", windInYerSails);
+    windInYerSails = [windInYerSails stringByAppendingFormat:@" %@!", sails];
+    NSLog(@"%@", windInYerSails);
+    
+    
+    
     
     
     // Do not alter
