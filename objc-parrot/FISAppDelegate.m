b53b053377f89b9867b4cbe8123fa28bd4080f2e
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-04 14:33:54 -0500

    Completes tasks for "Code-Along I: "Squawk!"

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -13,6 +13,24 @@
      
      */
     
+    // Code-Along I: "Squawk!"
+    
+    // A. NSLog() and the String Literal
+    
+    NSLog(@"Squawk!");
+    NSLog(@"%@", @"Squawk!");
+    NSLog(@"%@!", @"Squawk");
+    NSLog(@"%@!", [@"Squawk" uppercaseString]);
+    
+    // B. NSString Variable
+    
+    NSString *squawk = @"squawk";
+    NSLog(@"%@", squawk);
+    squawk = [squawk uppercaseString];
+    NSLog(@"%@", squawk);
+    squawk = [squawk stringByAppendingString:@"!"];
+    NSLog(@"%@", squawk);
+    
     // Do not alter
     return YES;  //
 }   ///////////////
