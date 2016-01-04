08ad27e5d52b44f50cb07d7d11f6debc1aba84bc
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-04 14:37:21 -0500

    Completes tasks for "Code-Along II: "Wind in yer sails!"

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -13,23 +13,33 @@
      
      */
     
-    // Code-Along I: "Squawk!"
+    // Code-Along II: "Wind in yer sails!"
     
-    // A. NSLog() and the String Literal
+    // A. Interpolate multiple strings into an NSLog()
     
-    NSLog(@"Squawk!");
-    NSLog(@"%@", @"Squawk!");
-    NSLog(@"%@!", @"Squawk");
-    NSLog(@"%@!", [@"Squawk" uppercaseString]);
+    NSString *wind = @"Wind";
+    NSString *inString = @"in";
+    NSString *yer = @"yer";
+    NSString *sails = @"sails";
+    NSLog(@"%@ %@ %@ %@!", wind, inString, yer, sails);
     
-    // B. NSString Variable
+    // B. Interpolate multiple strings into a string variable
     
-    NSString *squawk = @"squawk";
-    NSLog(@"%@", squawk);
-    squawk = [squawk uppercaseString];
-    NSLog(@"%@", squawk);
-    squawk = [squawk stringByAppendingString:@"!"];
-    NSLog(@"%@", squawk);
+    NSString *windInYerSails = [NSString stringWithFormat:@"%@ %@ %@ %@!", wind, inString, yer, sails];
+    NSLog(@"%@", windInYerSails);
+    
+    // C. Build a string by concatenating substrings
+    
+    NSLog(@"%@", wind);
+    windInYerSails = [NSString stringWithString:wind];
+    NSLog(@"%@", windInYerSails);
+    windInYerSails = [windInYerSails stringByAppendingString:@" "];
+    windInYerSails = [windInYerSails stringByAppendingString:inString];
+    NSLog(@"%@", windInYerSails);
+    windInYerSails = [windInYerSails stringByAppendingFormat:@" %@", yer];
+    NSLog(@"%@", windInYerSails);
+    windInYerSails = [windInYerSails stringByAppendingFormat:@" %@!", sails];
+    NSLog(@"%@", windInYerSails);
     
     // Do not alter
     return YES;  //
