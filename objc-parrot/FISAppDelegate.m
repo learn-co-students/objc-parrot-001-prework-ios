9120222423d75bf4c9a475ba9ca290acd771dcbc
Dougly <dgalante11@gmail.com>
2016-02-16 17:31:57 -0500

    code along III a b and c done

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -48,6 +48,45 @@
     windInYerSails = [windInYerSails stringByAppendingFormat:@" %@!", sails];
     NSLog(@"%@", windInYerSails);
     
+    NSString *look = @"look";
+    NSString *at = @"at";
+    NSString *me = @"me";
+    NSString *im = @"i'm";
+    NSString *molting = @"molting";
+    
+    NSLog(@"%@%@%@%@%@", look, at, me, im, molting);
+    NSLog(@"%@ %@ %@ %@ %@", look, at, me, im, molting);
+    NSLog(@"%@ %@ %@, %@ %@!", look, at, me, im, molting);
+    NSLog(@"%@ %@ %@, %@ %@!", [look uppercaseString], [at uppercaseString], [me uppercaseString], [im uppercaseString], [molting uppercaseString]);
+    
+    NSString *lookAt = [look stringByAppendingFormat:@" %@", at];
+    NSLog(@"%@", lookAt);
+    
+    NSString *lookAtMe = [lookAt stringByAppendingFormat:@" %@", me];
+    NSLog(@"%@", lookAtMe);
+    
+    lookAtMe = [lookAtMe uppercaseString];
+    NSLog(@"%@", lookAtMe);
+    
+    NSString *imMolting = [NSString stringWithFormat:@"%@ %@", im, molting];
+    NSLog(@"%@", imMolting);
+    
+    imMolting = [imMolting uppercaseString];
+    NSLog(@"%@", imMolting);
+    
+    NSString *lookAtMeImMolting = [lookAtMe stringByAppendingFormat:@", %@!", imMolting];
+    NSLog(@"%@", lookAtMeImMolting);
+    
+    NSString *iagoShout = [NSString stringWithFormat:@"%@ %@ %@ %@ %@", look, at, me, im, molting];
+    NSLog(@"%@", iagoShout);
+    
+    NSLog(@"%@", [iagoShout uppercaseString]);
+    
+    iagoShout = [iagoShout uppercaseString];
+    NSLog(@"%@", iagoShout);
+    
+    
+    
     
     
     
