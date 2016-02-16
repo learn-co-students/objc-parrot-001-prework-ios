b163e8c07e68a1f7202e0d4a01f0bff4fb51cfbd
Dougly <dgalante11@gmail.com>
2016-02-16 16:41:08 -0500

    A and B complete

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -7,11 +7,21 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     *  Write your code here!
-     
-     */
+    
+    
+    NSLog(@"Squawk!");
+    NSLog(@"%@", @"Squawk!");
+    NSLog(@"%@!", @"Squawk");
+    
+    NSLog(@"%@!", [@"squawk" uppercaseString]);
+    
+    NSString *squawk = @"squawk";
+    NSLog(@"%@", squawk);
+    squawk = [squawk uppercaseString];
+    NSLog(@"%@", squawk);
+    squawk = [squawk stringByAppendingString:@"!"];
+    NSLog(@"%@", squawk);
+    
     
     // Do not alter
     return YES;  //
