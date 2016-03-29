9ac8daeac1dc8c6950595731943899702ee1deeb
chriswebb09 <chris.webb5249@gmail.com>
2016-03-29 17:57:04 -0400

    Done.

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -7,11 +7,31 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     *  Write your code here!
-     
-     */
+    NSString *squawk = @"Squawk";
+    NSLog(@"%@!", squawk);
+    squawk = [squawk uppercaseString];
+    NSLog(@"%@!", squawk);
+    squawk = [squawk stringByAppendingString:@"!"];
+    NSLog(@"%@", squawk);
+    NSString *wind = @"Wind";
+    NSString *inString = @"in";
+    NSString *yer = @"yer";
+    NSString *sails = @"sails";
+    NSString *windInYerSails = [NSString stringWithFormat:@"%@ %@ %@ %@!", wind, inString, yer, sails];
+    NSLog(@"%@", windInYerSails);
+    NSString *look = @"look";
+    NSString *at = @"at";
+    NSString *me = @"me";
+    NSString *im = @"i'm";
+    NSString *molting = @"molting";
+    NSLog(@"%@ %@ %@ %@ %@", look, at, me, im, molting);
+    NSLog(@"%@ %@ %@, %@ %@!", [look uppercaseString], [at uppercaseString], [me uppercaseString], [im uppercaseString], [molting uppercaseString]);
+    NSString *lookAt = [look stringByAppendingFormat:@" %@", at];
+    NSString *lookAtMe = [lookAt stringByAppendingFormat:@" %@", me];
+    NSString *imMolting = [NSString stringWithFormat:@"%@ %@", im, molting];
+    NSLog(@"%@", imMolting);
+    NSString *lookAtMeImMolting = [lookAtMe stringByAppendingFormat:@" %@!", imMolting];
+    NSLog(@"%@", lookAtMeImMolting);
     
     // Do not alter
     return YES;  //
