95a3900aadfdfcba5893eeaba89d4378ecfb5dde
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2016-06-26 11:32:48 -0700

    squawking away

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -7,11 +7,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     *  Write your code here!
-     
-     */
+    NSLog(@"Squawk!");
+    NSLog(@"%@", @"Squawk!");
+    NSLog(@"%@!", @"Squawk");
+    NSLog(@"%@!", [@"Squawk" uppercaseString]);
+    NSString *squawk = @"squawk";
+    NSLog(@"%@", squawk);
+    
+    
+    
+    
+    
     
     // Do not alter
     return YES;  //
