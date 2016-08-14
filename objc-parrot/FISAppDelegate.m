e408a637ca8dc96275f270678087ccf442b17a0b
Michael <MH51893@aol.com>
2016-08-14 16:47:57 -0400

    Part A - NSLog and the String Literal

--- objc-parrot/FISAppDelegate.m
+++ objc-parrot/FISAppDelegate.m
@@ -7,11 +7,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     *  Write your code here!
-     
-     */
+    NSLog(@"Squawk!");
+    
+    NSLog(@"%@", @"Squawk!");
+    
+    NSLog(@"%@!", @"Squawk");
+    
+    NSLog(@"%@!", [@"Squawk" uppercaseString]);
     
     // Do not alter
     return YES;  //
